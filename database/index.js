ae68d90f47191476adba346188af70ccd7d10269
MattRodigheri <mrodigheri@gmail.com>
2018-09-06 19:32:55 -0700

    wrote post method to seed data table

--- database/index.js
+++ database/index.js
@@ -11,9 +11,29 @@ var getPhotos = function(callback) {
     if (err) {
       callback(err)
     } else {
-      callback(rows)
+      callback(null, rows)
     }
   })
 }
 
+// var insertInfo = function(callback) {
+//   function getRandomInt(min, max) {
+//     return Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min) + 1)) + Math.ceil(min);
+//   }
+//   var id = 0;
+//   var random = 1;
+//   while (id < 50) {
+//     random = getRandomInt(1, 100);
+//     id++;
+//     connection.query(`UPDATE photos SET reviews = ${random} WHERE id = ${id}`, function (err, rows, fields) {
+//       if (err) {
+//         callback(err);
+//       } else {
+//         callback(null);
+//       }
+//     });
+//   }
+// }
+//
+// module.exports.insertInfo = insertInfo;
 module.exports.getPhotos = getPhotos;
--- server/index.js
+++ server/index.js
@@ -3,18 +3,28 @@ const app = express();
 const dbMethods = require('../database/index.js');
 const bodyParser = require('body-parser');
 
-app.use(bodyParser(express.static(__dirname + './public/')))
-
+app.use(express.static(__dirname + './public/'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/photos', function(req, res) {
-   res.send('Hello World!');
-  // dbMethods.getPhotos = function(err, data) {
-  //   if (err) {
-  //     console.log(err);
-  //   } else {
-  //     res.send(data);
-  //   }
-  // }
+  dbMethods.getPhotos(function(err, data) {
+    if(err) {
+      res.sendStatus(503);
+    } else {
+      console.log(data)
+      res.send(data);
+    }
+  })
 })
+
+// app.post('/photos', function(req, res) {
+//   dbMethods.insertInfo(function(err, data) {
+//     if(err) {
+//       console.log(err)
+//     } else {
+//       console.log('posted!')
+//     }
+//   })
+// })
 
 app.listen(3000, () => console.log('listening on port 3000!'))
